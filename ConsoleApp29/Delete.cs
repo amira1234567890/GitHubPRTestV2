c29dc101e41fcae89516b3f10ed9d094544b96c6
Amira Natasha Binti Abdullah Harun <v-ambintiabd@microsoft.com>
2024-06-24 03:02:59 +0000

    add

---
+++ ConsoleApp29/Delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    internal class Delete
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void Method2()
+        {
+            this.Method();
+            this.Method();
+        }
+    }
+}
